c3ed46f0d6f4ad7984ac09249cb86ca258f84f0e
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-25 15:16:21 -0300

    se creo el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import FormularioTareas from "./components/FormularioTareas";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Container>
+      <Container className="main">
         <h1 className="display-1 text-center text-white my-3">Lista de Tareas</h1>
         <FormularioTareas></FormularioTareas>
       </Container>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="text-center bg-black py-3 text-white">
+             <p>&copy; Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
