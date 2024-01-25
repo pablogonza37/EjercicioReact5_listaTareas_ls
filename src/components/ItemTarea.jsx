2cee07648d6bee19ae3045b9f16f5682c4a56428
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-25 14:58:50 -0300

    se creo el compnente ItemTarea

---
+++ src/components/ItemTarea.jsx
@@ -0,0 +1,9 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const ItemTarea = () => {
+    return (
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+    );
+};
+
+export default ItemTarea;
