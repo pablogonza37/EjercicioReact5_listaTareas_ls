8456c1404d0f70ea178e8170b092578f52164d94
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-25 14:56:06 -0300

    se creo el componente ListaTareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,14 +1,23 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import ListaTareas from "./ListaTareas";
 
 const FormularioTareas = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3 d-flex justify-content-between" controlId="exampleForm.ControlInput1">
-        <Form.Control type="text" placeholder="Agregar Tarea" />
-        <Button variant="warning" className='mx-3'>Agregar</Button>
-      </Form.Group>
-    </Form>
+    <section>
+      <Form>
+        <Form.Group
+          className="mb-3 d-flex justify-content-between"
+          controlId="exampleForm.ControlInput1"
+        >
+          <Form.Control type="text" placeholder="Agregar Tarea" />
+          <Button variant="warning" className="mx-3">
+            Agregar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
+    </section>
   );
 };
 
---
+++ src/components/ListaTareas.jsx
@@ -0,0 +1,11 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const ListaTareas = () => {
+  return (
+    <ListGroup>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+    </ListGroup>
+  );
+};
+
+export default ListaTareas;
