1d8d7419c2d9ae8fcf7c897698501d58ba881e0d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-25 21:17:30 -0300

    se creó la funcion de agregar tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,22 +1,40 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTareas = () => {
+
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTareas([...tareas, tarea]);
+    setTarea("");
+  };
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group
           className="mb-3 d-flex justify-content-between"
           controlId="exampleForm.ControlInput1"
         >
-          <Form.Control type="text" placeholder="Agregar Tarea" />
-          <Button variant="warning" className="mx-3">
+          <Form.Control
+            type="text"
+            placeholder="Agregar Tarea"
+            minLength={3}
+            maxLength={50}
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="warning" className="mx-3" type="submit">
             Agregar
           </Button>
         </Form.Group>
       </Form>
-      <ListaTareas></ListaTareas>
+      <ListaTareas tareas={tareas}></ListaTareas>
     </section>
   );
 };
--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,9 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 
-const ItemTarea = () => {
+const ItemTarea = ({nombreTarea}) => {
     return (
-        <ListGroup.Item className='d-flex justify-content-between'>Cras justo odiobut<Button variant="danger">Borrar</Button></ListGroup.Item>
+        <ListGroup.Item className='d-flex justify-content-between'>{nombreTarea}<Button variant="danger">Borrar</Button></ListGroup.Item>
     );
 };
 
